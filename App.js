577e491876d86e55100b99dd46b0879e63a1bdcc
Duc Anh <hoangducanh12a8@gmail.com>
2025-09-15 19:15:02 +0700

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* Tiêu đề */}
+      <Text style={styles.title}>Programming Languages</Text>
+
+      {/* Danh sách hình ảnh cuộn ngang */}
+      <ScrollView horizontal showsHorizontalScrollIndicator={false} style={styles.scrollContainer}>
+        <View style={styles.imageWrapper}>
+          <Image source={require('./assets/Icon1.jpg')} style={styles.image} />
+        </View>
+        <View style={styles.imageWrapper}>
+          <Image source={require('./assets/Icon2.jpg')} style={styles.image} />
+        </View>
+        <View style={styles.imageWrapper}>
+          <Image source={require('./assets/Icon3.jpg')} style={styles.image} />
+        </View>
+        <View style={styles.imageWrapper}>
+          <Image source={require('./assets/Icon4.jpg')} style={styles.image} />
+        </View>
+        <View style={styles.imageWrapper}>
+          <Image source={require('./assets/Icon5.jpg')} style={styles.image} />
+        </View>
+        <View style={styles.imageWrapper}>
+          <Image source={require('./assets/Icon6.jpg')} style={styles.image} />
+        </View>
+      </ScrollView>
+
+
+      {/* Ô nhập văn bản */}
+      <TextInput
+        style={styles.input}
+        placeholder="What is your favorite programming language from the list above?"
+      />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +47,46 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  scrollContainer: {
+    maxHeight: 400,
+    marginTop: 50,
+
+  },
+  scrollContent: {
+    alignItems: 'center',
+  },
+  image: {
+    width: 250,
+    height: 250,
+    marginRight: 15,
+    borderRadius: 10,
+    borderColor: '#000',
+    borderWidth: 3,
+  },
+
+  imageWrapper: {
+    backgroundColor: 'orange',
+    borderColor: '#000',
+    borderRadius: 15,
+    marginRight: 10,
+    marginLeft: 10,
+    padding: 5,
+  },
+
+  input: {
+    borderWidth: 1,
+    borderColor: '#aaa',
+    borderRadius: 8,
+    padding: 10,
+    fontSize: 14,
+    width: '90%',
   },
 });
